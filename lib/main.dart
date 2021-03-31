01568f0a9c3028db0817bb768ea8f16ed1820224
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2021-03-31 10:02:06 +0200

    Appli Calc calories

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './customText.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,34 +29,46 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  // String changed;
+  double poids;
   // String submitted;
 
-  bool interrupteur = false;
-  double sliderDouble = 0.0;
+  bool genre = false;
+  double taille = 150;
+  int age;
 
-  int itemSelectionne;
+  int radioSelectionne;
 
   DateTime date;
 
+  Map activiteSport = {
+    1: "faible",
+    2: "modere",
+    3: "forte"
+  };
+
+  int calorieBase;
+  int calorieWithActivity;
+
   List<Widget> radios() {
     List<Widget> l = [];
-    for (int x = 0 ; x < 4 ; x++) {
-      Row row = Row(
+    activiteSport.forEach((key, value) { 
+      Column column = Column(
         children:<Widget>[
-          Text('Choix ${x + 1}'),
+          CustomText(value, color: setColor(), factor: 1.3),
           Radio(
-            value: x,
-            groupValue: itemSelectionne,
-            onChanged: (int i) {
+            activeColor: setColor(),
+            value: key,
+            groupValue: radioSelectionne,
+            onChanged: (Object i) {
               setState((){
-                itemSelectionne = i;
+                radioSelectionne = i;
               });
-            })
+            }
+          )
         ]
       );
-      l.add(row);
-    }
+      l.add(column);
+    });
     return l;
   }
 
@@ -90,6 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    double height = 500;
     // return GestureDetector(
     //   onTap: () {
     //     FocusScope.of(context).requestFocus(FocusNode());
@@ -122,43 +136,138 @@ class _MyHomePageState extends State<MyHomePage> {
     //     ),
     //   )
     // );
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: montrerDate,
-                child: Text(
-                  date == null ? 'Appuyer' : date.toString()
-                ))
-              // Text('Aimez-vous les haricots ?'),
-              // Slider(
-              //   value: sliderDouble,
-              //   min:0,
-              //   max: 10,
-              //   onChanged: (double d) {
-              //     setState(() {
-              //       sliderDouble = d;
-              //     });
-              //   }
-              // )
-              // Switch(
-              //   value: interrupteur,
-              //   inactiveTrackColor: Colors.orange,
-              //   activeTrackColor: Colors.blue,
-              //   onChanged: (bool b) {
-              //     setState(() {
-              //       interrupteur = b;
-              //     });
-              //   }
-              // )
-            ]
+    return 
+      GestureDetector(
+        onTap: (() => FocusScope.of(context).requestFocus(FocusNode())),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+            backgroundColor: setColor(),
+          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Center(
+                  child: CustomText(
+                          'Remplissez tous les champs pour remplir votre besoin journalier en calories.',
+                          factor: 1.2,
+                  ),
+                ),
+                Container(
+                  height: height,
+                    child: Card(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 20, right: 20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                CustomText(
+                                  'Homme',
+                                  color: Colors.blue,
+                                  factor: 1.3,
+                                ),
+                                Switch(
+                                  value: genre,
+                                  inactiveTrackColor: Colors.blue,
+                                  thumbColor: MaterialStateProperty.all(Colors.grey[100]),
+                                  activeTrackColor: Colors.pink,
+                                  onChanged: (bool b) {
+                                    setState(() {
+                                      genre = b;
+                                    });
+                                  }
+                                ),
+                                CustomText(
+                                  'Femme',
+                                  color: Colors.pink,
+                                  factor: 1.3,
+                                ),
+                              ]
+                            )
+                          ),
+                          TextButton(
+                            child: CustomText(date == null ? 'Appuyer' : calculAge(date), factor: 1.3, color: Colors.white),
+                            onPressed: () {
+                              montrerDate();
+                            },
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                                (Set<MaterialState> states) {
+                                  if (states.contains(MaterialState.pressed))
+                                    return setColor().withOpacity(0.5);
+                                  return setColor(); // Use the component's default.
+                                },
+                              ),
+                            ),
+                          ),
+                          CustomText(
+                            'Votre taille est de ${taille.floor()} cm',
+                          ),
+                          Slider(
+                            value: taille,
+                            activeColor: setColor(),
+                            min:100,
+                            max: 220,
+                            onChanged: (double d) {
+                              setState(() {
+                                taille = d;
+                              });
+                            }
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 20, right: 20),
+                            child: TextField(
+                              keyboardType: TextInputType.number,
+                              onChanged: (String string) {
+                                setState(() {
+                                  poids = double.tryParse(string);
+                                });
+                              },
+                              decoration: InputDecoration(
+                                labelText: "Entrez votre poids en kilos."
+                              ),
+                            ),
+                          ),
+                          CustomText(
+                            'Quelle est votre activité sportive',
+                            factor: 1.3
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: radios()
+                          ),
+                        ]
+                      )
+                  )
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    calcNumberCalories ();
+                  },
+                  child: Text(
+                    'Calculer'
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                      (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.pressed))
+                          return setColor().withOpacity(0.5);
+                        return setColor(); // Use the component's default.
+                      },
+                    ),
+                  ),
+                )
+              ]
+            ),
           )
         )
+      ) 
     );
   }
 
@@ -167,13 +276,100 @@ class _MyHomePageState extends State<MyHomePage> {
       context: context,
       initialDatePickerMode: DatePickerMode.year,
       initialDate: DateTime.now(),
-      firstDate: DateTime(1983),
-      lastDate: DateTime(2045)
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now()
     );
     if (choix != null) {
       setState(() {
         date = choix;
       });
+    }
+  }
+
+  String calculAge(date) {
+    setState(() {
+      age = (DateTime.now().difference(date).inDays / 365).floorToDouble().toInt();
+    });
+    return "Votre age est de : $age ans.";
+  }
+
+  void calcNumberCalories() {
+    if (date != null && poids != null && radioSelectionne != null) {
+      if (genre) {
+        calorieBase = (66.4730 + (13.7516 * poids) + (5.0033 * taille) - (6.7550 * age)).toInt();
+      } else {
+        calorieBase = (655.0955 + (9.5634 * poids) + (1.8496 * taille) - (4.6756 * age)).toInt();
+      }
+      switch(radioSelectionne) {
+        case 1:
+          calorieWithActivity = (calorieBase * 1.2).toInt();
+          break;
+        case 2:
+          calorieWithActivity = (calorieBase * 1.5).toInt();
+          break;
+        case 3:
+          calorieWithActivity = (calorieBase * 1.8).toInt();
+          break;
+        default:
+          calorieWithActivity = calorieBase;
+          break;
+      }
+      setState(() {
+        dialog();
+      });
+    } else {
+      alert();
+    }
+  }
+
+  Future<Null> dialog() async {
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext buildContext) {
+        return SimpleDialog(
+          title: CustomText('Votre besoin en calorie', color: setColor()),
+          contentPadding: EdgeInsets.all(15),
+          children: <Widget>[
+            CustomText('Votre besoin de base est de: $calorieBase'),
+            CustomText('Votre besoin avec activité sportive est de: $calorieWithActivity'),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.pop(buildContext);
+              },
+              child: CustomText("Ok", color: Colors.white)
+            ),
+          ],
+        );
+      }
+    );
+  }
+
+  Future<Null> alert() async {
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext buildContext) {
+        return AlertDialog(
+          title: CustomText('Erreur', color: Colors.red, factor: 1.5),
+          content: CustomText('Tous les champs ne sont pas remplis'),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: (){
+                Navigator.pop(buildContext);
+              },
+              child: CustomText("Ok")
+            ),
+          ],
+        );
+      });
+  }
+
+  Color setColor() {
+    if (genre) {
+      return Colors.pink;
+    } else {
+      return Colors.blue;
     }
   }
 }
